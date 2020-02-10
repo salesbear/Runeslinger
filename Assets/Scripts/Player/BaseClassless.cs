4b8635da71bbd85ffc35aae566363ea1e3eb2615
Vinson Kok <vinsonkok@yahoo.com>
2020-02-10 02:43:23 -0600

    Basic class for player stats

---
+++ Assets/Scripts/Player/BaseClassless.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BaseClassless : BaseCharacterClass
+{
+    public void Classless()
+    {
+        maxHealth = 30;
+        currentHealth = 30;
+
+        maxGrit = 4;
+        currentGrit = 4;
+        accuracyThisTurn = 0;
+        accuracyThisBattle = 0;
+        shield = 0;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    BaseClassless playerClass = new BaseClassless();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerClass.Classless();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(playerClass.maxHealth);
+    }
+}
