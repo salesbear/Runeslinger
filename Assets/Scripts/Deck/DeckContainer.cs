98a3f1ce081e37283eb7dfedd4821c237908e668
Marvin <nivramaxuf@gmail.com>
2020-02-17 15:33:36 -0600

    Prefabs and Randomization
    I turned all the current card assets into prefabs and set up random starting deck system in DeckContainer

--- Assets/Scripts/Deck/DeckContainer.cs
+++ Assets/Scripts/Deck/DeckContainer.cs
@@ -1,13 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class DeckContainer : MonoBehaviour
 {
     public GameObject[] deck = new GameObject[12];
-    public GameObject[] PossibleCards = new GameObject[12];
+    public GameObject[] StarterCards = new GameObject[2];
+    public GameObject[] CommonCards = new GameObject[4];
+    public GameObject[] UncommonCards = new GameObject[5];
+    public GameObject[] RareCards = new GameObject[3];
     public void Awake()
     {
+        int i = 0;
+        for (i = i; i < 4; i++)
+        {
+            deck[i] = StarterCards[0];
+        }
+        for (i = i; i < 8; i++)
+        {
+            deck[i] = StarterCards[1];
+        }
+        for(i = i; i < 11; i++)
+        {
+            deck[i] = CommonCards[UnityEngine.Random.Range(0, CommonCards.Length)];
+        }
+        deck[i] = UncommonCards[UnityEngine.Random.Range(0, UncommonCards.Length)];
+        //deck[i] = 
         //deck.Add
     }
 }
