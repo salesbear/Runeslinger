17374a5a82b233d72e5bb3ad0c4f5a5aae74b5a6
Vinson Kok <vinsonkok@yahoo.com>
2020-02-19 18:34:58 -0600

    Semi-fix to PreviewCard script (can currently still drag cards)

--- Assets/Scripts/Card/PreviewCard.cs
+++ Assets/Scripts/Card/PreviewCard.cs
@@ -6,6 +6,17 @@ public class PreviewCard : MonoBehaviour
 {
     public float enlargeX = 25;
     public float enlargeY = 25;
+    public float origXScale;
+    public float origYScale;
+
+    private void Start()
+    {
+        origXScale = transform.localScale.x;
+        origYScale = transform.localScale.y;
+
+        CardMove cardMove = GetComponent<CardMove>();
+        cardMove.enabled = false;
+    }
 
     public void OnMouseEnter()
     {
@@ -16,6 +27,6 @@ public class PreviewCard : MonoBehaviour
     public void OnMouseExit()
     {
         // assuming you want it to return to its original size when your mouse leaves it.
-        transform.localScale = new Vector3(50, 50, 1); 
+        transform.localScale = new Vector3(origXScale, origYScale, 1); 
     }
 }
