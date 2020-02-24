498149ade122700d3f7d301d3a5579e304c153a9
Travis Scharlach <travisscharlach@gmail.com>
2020-02-24 15:25:13 -0600

    Continued work on Damage Numbers before merging with master

--- Assets/Scripts/DmgNumbers.cs
+++ Assets/Scripts/DmgNumbers.cs
@@ -5,6 +5,7 @@ using TMPro;
 
 public class DmgNumbers : MonoBehaviour
 {
+    Rigidbody2D rb;
 
     [SerializeField] float hiRollAngle = 0f;
     [SerializeField] float loRollAngle = 50f; //in degrees
@@ -13,31 +14,41 @@ public class DmgNumbers : MonoBehaviour
     public float rolledAngle { get; private set; }
     public float rolledMagnitude { get; private set; }
     public TextMeshProUGUI dmgText { get; private set; }
-    public Color noDamageColor { get; private set; } = new Color32(150, 150, 150, 255);
-    public Color shieldDamageColor { get; private set; } = new Color32(108, 192, 221, 255);
+    public static Color damageColor { get; private set; } = new Color32(255, 0, 0, 255);
+    public static Color noDamageColor { get; private set; } = new Color32(150, 150, 150, 255);
+    public static Color shieldDamageColor { get; private set; } = new Color32(108, 192, 221, 255);
 
     void OnEnable()
     {
         rolledAngle = Random.Range(loRollAngle, hiRollAngle);
         rolledMagnitude = Random.Range(loRollMagn, hiRollMagn);
     }
-    
+
     void Awake()
     {
         dmgText = GetComponent<TextMeshProUGUI>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     //replaces the text in the prefab with a number or whatever you want, really
+    //changes the color of the text to whatever you want, three colors are provided above.
     //Default text is "0"
     //Default Color is Red
-    void WriteDamage(int number)
-    {
-        dmgText.text = number.ToString();
-    }
-    //overload to change the color of the number (for no damage or blocked damage);
-    void WriteDamage(int number, Color color)
+    public void WriteDamage(int number, Color color)
     {
         dmgText.text = number.ToString();
         dmgText.color = color;
     }
+
+    public void Launch()
+    {
+        Vector2 gamingVector = AngleToVector2(rolledAngle, rolledMagnitude);
+        rb.velocity = gamingVector;
+    }
+
+    private Vector2 AngleToVector2(float angle, float magnitude)
+    {
+        return (new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * magnitude);
+    }
+
 }
--- Assets/Scripts/Enemy/EnemyDisplay.cs
+++ Assets/Scripts/Enemy/EnemyDisplay.cs
@@ -18,6 +18,7 @@ public class EnemyDisplay : MonoBehaviour, IDamagable
     public Slider enemyHPBar;
     public Slider enemyShield;
     public TextMeshProUGUI rolledDMG;
+    GameObject damageNumber = Resources.Load<GameObject>("DamageNumbers");
 
 
     [Header("Image Stuff")]
@@ -36,12 +37,14 @@ public class EnemyDisplay : MonoBehaviour, IDamagable
     {
         enemy.EnemyUIUpdate += UpdateUI;
         enemy.Die += OnDie;
+        enemy.DamageNumber += OnDamage;
     }
 
     private void OnDisable()
     {
         enemy.EnemyUIUpdate -= UpdateUI;
         enemy.Die -= OnDie;
+        enemy.DamageNumber -= OnDamage;
     }
     //register with enemy controller on awake
     void Awake()
@@ -70,6 +73,14 @@ public class EnemyDisplay : MonoBehaviour, IDamagable
         //take self off enemy controller list on death
         enemyController.RemoveEnemy(this);
         this.gameObject.SetActive(false);
+    }
+
+    void OnDamage(int damage, Color color)
+    {
+        GameObject dmg = Instantiate(damageNumber, gameObject.transform.position, Quaternion.identity);
+        DmgNumbers dmgNum = dmg.GetComponent<DmgNumbers>();
+        dmgNum.WriteDamage(damage, color);
+        dmgNum.Launch();
     }
 
     void ReadEnemyFromAsset()
