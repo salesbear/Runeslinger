341a0ca94cf28706e3610788da198fbcc383f85e
Travis Scharlach <travisscharlach@gmail.com>
2020-03-04 19:45:22 -0600

    Finishing Touches

--- Assets/Scripts/Player/PlayerUI.cs
+++ Assets/Scripts/Player/PlayerUI.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using DG.Tweening;
+using UnityEngine.UI;
+
 
 public class PlayerUI : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI health;
+    [SerializeField] Slider healthBar;
     [SerializeField] TextMeshProUGUI grit;
+    [SerializeField] GameObject accuracyIndicator;
     [SerializeField] TextMeshProUGUI accuracy;
+    [SerializeField] GameObject shieldBar;
     [SerializeField] TextMeshProUGUI shield;
 
     public float enlargedScale;
@@ -25,49 +30,73 @@ public class PlayerUI : MonoBehaviour
     {
         if (PlayerStats.instance != null)
         {
-            health.text = "Health: " + PlayerStats.instance.playerClass.currentHealth;
-            grit.text = "Grit: " + PlayerStats.instance.playerClass.currentGrit;
-            accuracy.text = "Acc: " + PlayerStats.instance.playerClass.accuracy;
-            shield.text = "Shield: " + PlayerStats.instance.playerClass.shield;
+            health.text = PlayerStats.instance.playerClass.currentHealth.ToString() + "/" + PlayerStats.instance.playerClass.maxHealth.ToString();
+            healthBar.maxValue = PlayerStats.instance.playerClass.maxHealth;
+            healthBar.value = PlayerStats.instance.playerClass.currentHealth;
+            grit.text = PlayerStats.instance.playerClass.currentGrit.ToString() + "/" + PlayerStats.instance.playerClass.maxGrit.ToString();
+            if (PlayerStats.instance.playerClass.accuracy == 0)
+            {
+                accuracyIndicator.SetActive(false);
+            }
+            else
+            {
+                accuracyIndicator.SetActive(true);
+                accuracy.text = PlayerStats.instance.playerClass.accuracy.ToString();
+            }
+            if (PlayerStats.instance.playerClass.shield == 0)
+            {
+                shieldBar.SetActive(false);
+            }
+            else
+            {
+                shieldBar.SetActive(true);
+                shield.text = PlayerStats.instance.playerClass.shield.ToString();
+            }
         }
     }
 
     public void PopHealthText()
     {
+        /*
         RectTransform t = health.GetComponent<RectTransform>();
         t.DOScaleX(enlargedScale, duration);
         t.DOScaleY(enlargedScale, duration);
         StartCoroutine(ShrinkText(t, duration));
+        */
     }
 
     public void PopGritText()
     {
         RectTransform t = grit.GetComponent<RectTransform>();
+        Vector3 originalScale = t.localScale;
         t.DOScaleX(enlargedScale, duration);
         t.DOScaleY(enlargedScale, duration);
-        StartCoroutine(ShrinkText(t, duration));
+        StartCoroutine(ShrinkText(t, duration, originalScale));
     }
 
     public void PopAccuracyText()
     {
         RectTransform t = accuracy.GetComponent<RectTransform>();
+        Vector3 originalScale = t.localScale;
         t.DOScaleX(enlargedScale, duration);
         t.DOScaleY(enlargedScale, duration);
-        StartCoroutine(ShrinkText(t, duration));
+        StartCoroutine(ShrinkText(t, duration, originalScale));
     }
 
     public void PopShieldText()
     {
+        /*
         RectTransform t = shield.GetComponent<RectTransform>();
         t.DOScaleX(enlargedScale, duration);
         t.DOScaleY(enlargedScale, duration);
         StartCoroutine(ShrinkText(t, duration));
+        */
     }
 
-    IEnumerator ShrinkText(RectTransform t, float duration)
+    IEnumerator ShrinkText(RectTransform t, float duration, Vector3 scale)
     {
         yield return new WaitForSeconds(duration);
-        t.DOScaleX(1, duration);
-        t.DOScaleY(1, duration);
+        t.DOScaleX(scale.x, duration);
+        t.DOScaleY(scale.y, duration);
     }
 }
